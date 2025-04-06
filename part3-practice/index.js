d2969595fef9b0d5d2f6eb67c5e00830003a1ccc
mickel_arroz <111148461+mickel-arroz@users.noreply.github.com>
2025-04-05 21:14:09 -0400

    Initial practice part 3b

--- part3-practice/index.js
+++ part3-practice/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
