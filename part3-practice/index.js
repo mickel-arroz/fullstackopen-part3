a9ab937b6a50986ea7167b1042d4b88289224f3b
mickel_arroz <111148461+mickel-arroz@users.noreply.github.com>
2025-05-01 21:25:13 -0400

    Practice - Update Schema Note Validations

--- part3-practice/index.js
+++ part3-practice/index.js
@@ -71,14 +71,14 @@ app.delete("/api/notes/:id", (request, response, next) => {
 });
 
 app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
+  Note.findByIdAndUpdate(
+    request.params.id,
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    { content, important },
+    { new: true, runValidators: true, context: "query" }
+  )
     .then((updatedNote) => {
       response.json(updatedNote);
     })
@@ -90,21 +90,21 @@ const generateId = () => {
   return maxId + 1;
 };
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: "content missing" });
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -118,6 +118,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    console.error("ERROR VALIDACION");
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
